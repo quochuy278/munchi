da6d46f6d9ee06d59c511bb618ec60ff03e86c31
quochuy278 <quochuy270800@gmail.com>
2022-07-04 13:57:45 +0300

    add style set up

--- theme/themeConfig.js
+++ theme/themeConfig.js
@@ -1,120 +1,18 @@
 import { extendTheme } from "native-base";
+import colors from "./colors";
+import shadows from "./shadows";
+import sizes from "./sizes";
+import spacing  from "./space";
+import typography from "./typography ";
+import opacity from "./opacity";
 
-const ThemeConfig = extendTheme({
-  //Native base default will take 600 for every parameters in colors
-  colors: {
-    primary: {
-      500: "#db2777",
-      600: "#FF3537", //Main theme
-      700: "#9d174d",
-    },
-    black: {
-      600: "#000000",
-    },
-    grey: {
-      50: "#FAFAFA",
-      100: "#F5F5F5",
-      200: "#EEEEEE",
-      300: "#E0E0E0",
-      400: "#BDBDBD",
-      500: "#9E9E9E",
-      600: "#757575",
-      700: "#616161",
-      800: "#424242",
-      900: "#212121",
-      A100: "#D5D5D5",
-      A200: "#AAAAAA",
-      A400: "#616161",
-      A700: "#303030",
-    },
-    fontConfig: {
-      Roboto: {
-        100: {
-          normal: "Roboto-Light",
-          italic: "Roboto-LightItalic",
-        },
-        200: {
-          normal: "Roboto-Light",
-          italic: "Roboto-LightItalic",
-        },
-        300: {
-          normal: "Roboto-Light",
-          italic: "Roboto-LightItalic",
-        },
-        400: {
-          normal: "Roboto-Regular",
-          italic: "Roboto-Italic",
-        },
-        500: {
-          normal: "Roboto-Medium",
-        },
-        600: {
-          normal: "Roboto-Medium",
-          italic: "Roboto-MediumItalic",
-        },
-        // Add more variants
-        700: {
-          normal: "Roboto-Bold",
-        },
-        800: {
-          normal: "Roboto-Bold",
-          italic: "Roboto-BoldItalic",
-        },
-        900: {
-          normal: "Roboto-Bold",
-          italic: "Roboto-BoldItalic",
-        },
-      },
-      Mulish: {
-        100: {
-          normal: "Mulish-Light",
-          italic: "Mulish-LightItalic",
-        },
-        200: {
-          normal: "Mulish-Light",
-          italic: "Mulish-LightItalic",
-        },
-        300: {
-          normal: "Mulish-Light",
-          italic: "Mulish-LightItalic",
-        },
-        400: {
-          normal: "Mulish-Regular",
-          italic: "Mulish-Italic",
-        },
-        500: {
-          normal: "Mulish-Medium",
-        },
-        600: {
-          normal: "Mulish-Medium",
-          italic: "Mulish-MediumItalic",
-        },
-        // Add more variants
-        700: {
-          normal: "Mulish-Bold",
-        },
-        800: {
-          normal: "Mulish-Bold",
-          italic: "Mulish-BoldItalic",
-        },
-        900: {
-          normal: "Mulish-Bold",
-          italic: "Mulish-BoldItalic",
-        },
-      },
-    },
-
-    // Make sure values below matches any of the keys in `fontConfig`
-    fonts: {
-      heading: "Roboto",
-      body: "Roboto",
-      mono: "Mulish",
-    },
-    config: {
-      // Changing initialColorMode to 'dark'
-      initialColorMode: "dark",
-    },
-  },
+const theme = extendTheme({
+  colors,
+  shadows,
+  sizes,
+  spacing,
+  typography,
+  opacity
 });
 
-export default ThemeConfig;
+export default theme;
---
+++ theme/typography .js
@@ -0,0 +1,173 @@
+const typography = {
+  letterSpacings: {
+    xs: "-0.05em",
+    sm: "-0.025em",
+    md: 0,
+    lg: "0.025em",
+    xl: "0.05em",
+    "2xl": "0.1em",
+  },
+  lineHeights: {
+    "2xs": "1em",
+    xs: "1.125em",
+    sm: "1.25em",
+    md: "1.375em",
+    lg: "1.5em",
+    xl: "1.75em",
+    "2xl": "2em",
+    "3xl": "2.5em",
+    "4xl": "3em",
+    "5xl": "4em",
+  },
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+    extrablack: 950,
+  },
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+    mono: "Mulish",
+  },
+  fontConfig: {
+    Roboto: {
+      100: {
+        normal: "Roboto-Light",
+        italic: "Roboto-LightItalic",
+      },
+      200: {
+        normal: "Roboto-Light",
+        italic: "Roboto-LightItalic",
+      },
+      300: {
+        normal: "Roboto-Light",
+        italic: "Roboto-LightItalic",
+      },
+      400: {
+        normal: "Roboto-Regular",
+        italic: "Roboto-Italic",
+      },
+      500: {
+        normal: "Roboto-Medium",
+        italic: "Roboto-MediumItalic",
+      },
+      600: {
+        normal: "Roboto-Medium",
+        italic: "Roboto-MediumItalic",
+      },
+      700: {
+        normal: "Roboto-Bold",
+        italic: "Roboto-BoldItalic",
+      },
+      800: {
+        normal: "Roboto-Bold",
+        italic: "Roboto-BoldItalic",
+      },
+      900: {
+        normal: "Roboto-Bold",
+        italic: "Roboto-BoldItalic",
+      },
+    },
+    Montserrat: {
+      100: {
+        normal: "Montserrat-Light",
+        italic: "Montserrat-LightItalic",
+      },
+      200: {
+        normal: "Montserrat-Light",
+        italic: "Montserrat-LightItalic",
+      },
+      300: {
+        normal: "Montserrat-Light",
+        italic: "Montserrat-LightItalic",
+      },
+      400: {
+        normal: "Montserrat-Regular",
+        italic: "Montserrat-Italic",
+      },
+      500: {
+        normal: "Montserrat-Medium",
+        italic: "Montserrat-MediumItalic",
+      },
+      600: {
+        normal: "Montserrat-SemiBold",
+        italic: "Montserrat-SemiBoldItalic",
+      },
+      700: {
+        normal: "Montserrat-Bold",
+        italic: "Montserrat-BoldItalic",
+      },
+      800: {
+        normal: "Montserrat-ExtraBold",
+        italic: "Montserrat-ExtraBoldItalic",
+      },
+      900: {
+        normal: "Montserrat-Black",
+        italic: "Montserrat-BlackItalic",
+      },
+      
+    },
+    Mulish: {
+        100: {
+          normal: "Mulish-Light",
+          italic: "Mulish-LightItalic",
+        },
+        200: {
+          normal: "Mulish-Light",
+          italic: "Mulish-LightItalic",
+        },
+        300: {
+          normal: "Mulish-Light",
+          italic: "Mulish-LightItalic",
+        },
+        400: {
+          normal: "Mulish-Regular",
+          italic: "Mulish-Italic",
+        },
+        500: {
+          normal: "Mulish-Medium",
+        },
+        600: {
+          normal: "Mulish-Medium",
+          italic: "Mulish-MediumItalic",
+        },
+        // Add more variants
+        700: {
+          normal: "Mulish-Bold",
+        },
+        800: {
+          normal: "Mulish-Bold",
+          italic: "Mulish-BoldItalic",
+        },
+        900: {
+          normal: "Mulish-Bold",
+          italic: "Mulish-BoldItalic",
+        },
+    }
+  },
+  fontSizes: {
+    "2xs": 10,
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 20,
+    "2xl": 24,
+    "3xl": 30,
+    "4xl": 36,
+    "5xl": 48,
+    "6xl": 60,
+    "7xl": 72,
+    "8xl": 96,
+    "9xl": 128,
+  },
+};
+
+export default typography;
